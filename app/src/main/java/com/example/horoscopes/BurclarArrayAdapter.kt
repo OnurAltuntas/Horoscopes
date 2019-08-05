fe0e425361a418183226be2585c81854e4f75d51
Onur Altuntas <onuraltuntas50@gmail.com>
2019-08-05 18:09:26 +0300

    Custom ArrayAdapter getView added.

--- app/src/main/java/com/example/horoscopes/BurclarArrayAdapter.kt
+++ app/src/main/java/com/example/horoscopes/BurclarArrayAdapter.kt
@@ -1,8 +1,34 @@
 package com.example.horoscopes
 
 import android.content.Context
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.single_line2.view.*
 
-class BurclarArrayAdapter(context: Context, resource: Int, textViewResourceId: Int,txtHoroscopes:Array<String>,txtHoroscopeDate:Array<String>,burcImg:Array<Int>) :
-    ArrayAdapter<String>(context, resource, textViewResourceId,txtHoroscopes) {
+class BurclarArrayAdapter( var gelencontext: Context, resource: Int, textViewResourceId: Int, var txtHoroscopes:Array<String>,var txtHoroscopeDate:Array<String>,var burcImg:Array<Int>) :
+    ArrayAdapter<String>(gelencontext, resource, textViewResourceId,txtHoroscopes) {
+
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var inflater=LayoutInflater.from(gelencontext)
+        var singleline_view=inflater.inflate(R.layout.single_line2,parent,false)
+
+        var burcImgview=singleline_view.imgHoroscopes
+        var burcAdi=singleline_view.txtHoroscopes
+        var burcTarih=singleline_view.txtHoroscopeDate
+
+        burcImgview.setImageResource(burcImg[position])
+        burcAdi.setText(txtHoroscopes[position])
+        burcTarih.setText(txtHoroscopeDate[position])
+
+
+        return singleline_view
+    }
+
+
 }
--- app/src/main/java/com/example/horoscopes/MainActivity.kt
+++ app/src/main/java/com/example/horoscopes/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         var burclar= resources.getStringArray(R.array.burclar)
         var burcTarihler=resources.getStringArray(R.array.burcTarih)
-        var burcImg= arrayOf(R.drawable.koc1,R.drawable.koc1,R.drawable.boga2,R.drawable.ikizler3,R.drawable.yengec4,
+        var burcImg= arrayOf(R.drawable.koc1,R.drawable.boga2,R.drawable.ikizler3,R.drawable.yengec4,
             R.drawable.aslan5,R.drawable.basak6,R.drawable.terazi7,R.drawable.akrep8,R.drawable.yay9,R.drawable.oglak10,R.drawable.kova11,R.drawable.balik12)
 
         var burclarArrayAdapter=BurclarArrayAdapter(this,R.layout.single_line2,R.id.txtHoroscopes,burclar,burcTarihler,burcImg)
